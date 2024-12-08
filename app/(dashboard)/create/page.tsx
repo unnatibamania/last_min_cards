3b44cad17b7ff5e17a89820f5b6269ada61b2e1a
Unnati Bamania <unnatibamania8@gmail.com>
2024-12-08 17:08:44 +0530

    add navbar

--- app/(dashboard)/create/page.tsx
+++ app/(dashboard)/create/page.tsx
@@ -21,31 +21,33 @@ const CreatePage = () => {
             <AICards />
           )
         ) : (
-          <div className="border-2 border-dashed bg-white rounded-lg min-w-96 h-64 shadow-md">
-            <div className="flex flex-col items-center h-full justify-center gap-6">
-              <div className="flex flex-col items-center gap-1">
-                <h1 className="text-2xl font-bold">Create a new set</h1>
-                <p className="text-sm text-gray-500 max-w-64 text-center">
-                  Create your own set of cards or let AI generate them for you
-                </p>
-              </div>
+          <section className="flex items-center w-full h-full justify-center">
+            <div className="border-2 border-dashed bg-white rounded-lg min-w-96 h-64 shadow-md">
+              <div className="flex flex-col items-center h-full justify-center gap-6">
+                <div className="flex flex-col items-center gap-1">
+                  <h1 className="text-2xl font-bold">Create a new set</h1>
+                  <p className="text-sm text-gray-500 max-w-64 text-center">
+                    Create your own set of cards or let AI generate them for you
+                  </p>
+                </div>
 
-              <div className="flex gap-2">
-                <Button size={"lg"} onClick={() => setSelectedMethod("own")}>
-                  <Pencil className="w-4 h-4" />
-                  <span> Create</span>
-                </Button>
-                <Button
-                  variant="outline"
-                  onClick={() => setSelectedMethod("ai")}
-                  size={"lg"}
-                >
-                  <Stars className="w-4 h-4" />
-                  <span> Use AI</span>
-                </Button>
+                <div className="flex gap-2">
+                  <Button size={"lg"} onClick={() => setSelectedMethod("own")}>
+                    <Pencil className="w-4 h-4" />
+                    <span> Create</span>
+                  </Button>
+                  <Button
+                    variant="outline"
+                    onClick={() => setSelectedMethod("ai")}
+                    size={"lg"}
+                  >
+                    <Stars className="w-4 h-4" />
+                    <span> Use AI</span>
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
+          </section>
         )}
       </div>
     </div>
--- components/aside/Aside.tsx
+++ components/aside/Aside.tsx
@@ -1,5 +1,13 @@
 import { Button } from "../ui/button";
-import { HomeIcon, Settings } from "lucide-react";
+import {
+  HomeIcon,
+  Settings,
+  LucideDraftingCompass,
+  Notebook,
+  Stars,
+  Send,
+  Zap,
+} from "lucide-react";
 
 export const Aside = () => {
   return (
@@ -7,18 +15,55 @@ export const Aside = () => {
       <div className="flex flex-col items-center gap-4 p-2">
         <h1 className="text-2xl font-bold p-2">Sheep Ed App</h1>
 
-        <section className="flex flex-col gap-4 w-full">
-          <Button variant="ghost" className="w-full border justify-start">
+        <section className="flex flex-col gap-1 w-full">
+          <Button variant="ghost" className="w-full  justify-start">
             <HomeIcon className="w-4 h-4" />
             <span>Dashboard</span>
+          </Button>
+
+          <Button variant="ghost" className="w-full justify-between">
+            <div className="flex items-center gap-1">
+              <LucideDraftingCompass className="w-4 h-4" />
+              <span>Drafts</span>
+            </div>
+
+            <p>5</p>
+          </Button>
+
+          <Button variant="ghost" className="w-full  justify-between">
+            <div className="flex items-center gap-1">
+              <Notebook className="w-4 h-4" />
+              <span>My sets</span>
+            </div>
+
+            <p>5</p>
+          </Button>
+
+          <Button variant="ghost" className="w-full  justify-between">
+            <div className="flex items-center gap-1">
+              <Send className="w-4 h-4" />
+              <span>Shared</span>
+            </div>
+
+            <p>5</p>
+          </Button>
+
+          <Button variant="ghost" className="w-full  justify-start">
+            <Stars className="w-4 h-4" />
+            AI Generated
           </Button>
         </section>
       </div>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col p-4 gap-4">
         <Button variant="ghost" className="w-full justify-start">
           <Settings className="w-4 h-4" />
           <span>Settings</span>
+        </Button>
+
+        <Button className="w-full justify-start">
+          <Zap className="w-4 h-4" />
+          Upgrade account
         </Button>
       </div>
     </aside>
