f035075bdcd68c87dcd9572652dd94ce709c56d9
Unnati Bamania <unnatibamania8@gmail.com>
2024-12-15 12:18:41 +0530

    cards ui

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,7 +4,8 @@ import { RecentCard } from "@/components/cards/RecentCard";
 
 export default function Home() {
   return (
-    <div className="flex h-full flex-col p-6">
+    // <div className="flex justify-center w-full h-full">
+    <div className="flex h-full max-w-6xl gap-6 flex-col p-6">
       <div className="flex flex-col gap-4">
         <div className="flex flex-col">
           <h1 className="text-2xl font-bold">Recents</h1>
@@ -22,12 +23,19 @@ export default function Home() {
 
       <div className="flex flex-col gap-4">
         <div className="flex flex-col">
-          <h1 className="text-2xl font-bold">Recents</h1>
+          <h1 className="text-2xl font-bold">Popular cards</h1>
           <p className="text-sm text-gray-400">
             These are the cards which you have preffered last week
           </p>
         </div>
+
+        <div className="grid grid-cols-3 gap-2">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <RecentCard index={index} key={index} />
+          ))}
+        </div>
       </div>
     </div>
+    // </div>
   );
 }
