508925b4b8b2fe823b822677e244b867dba6ed3a
Unnati Bamania <unnatibamania@MacBook-Pro-2.local>
2024-12-25 11:43:29 +0530

    show cards and drafts count

--- components/aside/Aside.tsx
+++ components/aside/Aside.tsx
@@ -1,91 +1,104 @@
-"use client";
+// "use client";
 
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 
-import { Button } from "../ui/button";
-import { useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
-import {
-  HomeIcon,
-  Settings,
-  LucideDraftingCompass,
-  Notebook,
-  // Stars,
-  // Send,
-  Zap,
-} from "lucide-react";
+// import { Button } from "../ui/button";
+// import { useRouter } from "next/navigation";
+// import { cn } from "@/lib/utils";
+// import {
+//   HomeIcon,
+//   Settings,
+//   LucideDraftingCompass,
+//   Notebook,
+//   // Stars,
+//   // Send,
+//   Zap,
+// } from "lucide-react";
 
-export const Aside = () => {
-  const router = useRouter();
-  const pathname = usePathname();
+// export const Aside = () => {
+//   const router = useRouter();
+//   const pathname = usePathname();
 
-  return (
-    <aside className="flex flex-col border min-w-56 justify-between">
-      <div className="flex flex-col items-center gap-4 p-2">
-        <h1 className="text-2xl font-bold p-2">Sheep Ed App</h1>
+//   return (
+//     <aside className="flex flex-col border min-w-56 justify-between">
+//       <div className="flex flex-col items-center gap-4 p-2">
+//         <h1 className="text-2xl font-bold p-2">Sheep Ed App</h1>
 
-        <section className="flex flex-col gap-1 w-full">
-          <Button
-            variant="ghost"
-            onClick={() => router.push("/")}
-            className={cn(
-              "w-full justify-start",
-              pathname === "/" && "bg-muted"
-            )}
-          >
-            <HomeIcon className="w-4 h-4" />
-            <span>Dashboard</span>
-          </Button>
+//         <section className="flex flex-col gap-1 w-full">
+//           <Button
+//             variant="ghost"
+//             onClick={() => router.push("/")}
+//             className={cn(
+//               "w-full justify-start",
+//               pathname === "/" && "bg-muted"
+//             )}
+//           >
+//             <HomeIcon className="w-4 h-4" />
+//             <span>Dashboard</span>
+//           </Button>
 
-          <Button
-            variant="ghost"
-            onClick={() => router.push("/drafts")}
-            className={cn(
-              "w-full justify-between",
-              pathname === "/drafts" && "bg-muted"
-            )}
-          >
-            <div className="flex items-center gap-1">
-              <LucideDraftingCompass className="w-4 h-4" />
-              <span>Drafts</span>
-            </div>
+//           <Button
+//             variant="ghost"
+//             onClick={() => router.push("/drafts")}
+//             className={cn(
+//               "w-full justify-between",
+//               pathname === "/drafts" && "bg-muted"
+//             )}
+//           >
+//             <div className="flex items-center gap-1">
+//               <LucideDraftingCompass className="w-4 h-4" />
+//               <span>Drafts</span>
+//             </div>
 
-            <p>5</p>
-          </Button>
+//             <p>5</p>
+//           </Button>
 
-          <Button
-            onClick={() => router.push("/my-sets")}
-            variant="ghost"
-            className={cn(
-              "w-full  justify-between",
-              pathname === "/my-sets" && "bg-muted"
-            )}
-          >
-            <div className="flex items-center gap-1">
-              <Notebook className="w-4 h-4" />
-              <span>My sets</span>
-            </div>
+//           <Button
+//             onClick={() => router.push("/my-sets")}
+//             variant="ghost"
+//             className={cn(
+//               "w-full  justify-between",
+//               pathname === "/my-sets" && "bg-muted"
+//             )}
+//           >
+//             <div className="flex items-center gap-1">
+//               <Notebook className="w-4 h-4" />
+//               <span>My sets</span>
+//             </div>
 
-            <p>5</p>
-          </Button>
-        </section>
-      </div>
+//             <p>5</p>
+//           </Button>
+//         </section>
+//       </div>
 
-      <div className="flex flex-col p-4 gap-4">
-        <Button
-          onClick={() => router.push("/settings")}
-          variant="ghost"
-          className="w-full justify-start"
-        >
-          <Settings className="w-4 h-4" />
-          <span>Settings</span>
-        </Button>
+//       <div className="flex flex-col p-4 gap-4">
+//         <Button
+//           onClick={() => router.push("/settings")}
+//           variant="ghost"
+//           className="w-full justify-start"
+//         >
+//           <Settings className="w-4 h-4" />
+//           <span>Settings</span>
+//         </Button>
 
-        <Button className="w-full justify-start">
-          <Zap className="w-4 h-4" />
-          Upgrade account
-        </Button>
-      </div>
-    </aside>
-  );
+//         <Button className="w-full justify-start">
+//           <Zap className="w-4 h-4" />
+//           Upgrade account
+//         </Button>
+//       </div>
+//     </aside>
+//   );
+// };
+
+"use server";
+
+import { getMySets, getDraftSets } from "@/actions/set";
+
+import { Sidebar } from "./Sidebar";
+
+export const Aside = async () => {
+  const sets = await getMySets();
+  const draftSets = await getDraftSets();
+
+  return <Sidebar sets={sets.length} draftSets={draftSets.length} />;
 };
